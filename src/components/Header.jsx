cbe4c281342cb7752c11f0ce95f2d0386fe64f75
Austin O <guitarguy199@gmail.com>
2023-09-15 15:39:59 -0600

    style and layout improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 import './Header.css';
 import Logo from '../assets/tangentlogo.png';
 
@@ -6,22 +7,30 @@ const Header = () => {
     return (
         <header>
             <nav className='grid container navbar'>
-            <div className='navbar-logo'>
+            <Link className='navbar-logo' to={"/"}>
                 <img src={Logo} />
-                </div>
+            </Link>
                 <ul className='navbar-links'>
-                    <a href="/">Home</a>
-                    <a href="/">Home</a>
-                    <a href="/">Home</a>
-                    <a href="/">Home</a>
+                    <CustomLink to="/">Home</CustomLink>
+                    <CustomLink to="/">About</CustomLink>
+                    <CustomLink to="/">Services</CustomLink>
+                    <CustomLink to="/">Contact</CustomLink>
                 </ul>
             </nav>
         </header>
     )
 }
 
-function CustomLink() {
-    //add router link functionality
-}
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true})
+    return (
+      <li className={isActive ? "active" : "" }>
+        <Link to={to} aria-label={children} {...props}>
+          {children}
+        </Link>
+      </li>
+    )
+  }
 
 export default Header;
