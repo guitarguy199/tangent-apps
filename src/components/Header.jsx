44dc078b0c83cbe4549b4f1b4a4d36db43051be1
Austin O <guitarguy199@gmail.com>
2023-09-15 12:04:47 -0600

    layout and initial styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,27 @@
-
+import './Header.css';
+import Logo from '../assets/tangentlogo.png';
 
 const Header = () => {
 
     return (
         <header>
-            <nav>
-                <h4>Header</h4>
+            <nav className='grid container navbar'>
+            <div className='navbar-logo'>
+                <img src={Logo} />
+                </div>
+                <ul className='navbar-links'>
+                    <a href="/">Home</a>
+                    <a href="/">Home</a>
+                    <a href="/">Home</a>
+                    <a href="/">Home</a>
+                </ul>
             </nav>
         </header>
     )
+}
+
+function CustomLink() {
+    //add router link functionality
 }
 
 export default Header;
