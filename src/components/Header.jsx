06e1b2ecd5a932b2a4f996ffd8a9e8a8b3eed3fe
Austin O <guitarguy199@gmail.com>
2023-09-15 11:41:56 -0600

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+const Header = () => {
+
+    return (
+        <header>
+            <nav>
+                <h4>Header</h4>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
