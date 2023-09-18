1048e363393de0ec600f6e1ae58501024295bcf2
Austin O <guitarguy199@gmail.com>
2023-09-18 10:51:13 -0600

    fix apostrophe warning

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className='grid container'>
                 <img src={Logo} alt="Tangent Apps Logo" className='hero-image' />
             <div className='hero-text'>
-                <h1>Hi, We're Tangent Apps</h1>
+                <h1>Hi, We&apos;re Tangent Apps</h1>
                 <h2>A Modern Technology Consulting Agency</h2>
                 <h2>Based in Denver, CO.</h2>
                 <p>We build one-stop, intuitive technology solutions for small- to medium-sized businesses. We handle everything from web and app development to marketing, accounting, and customer relations technology, all with a focus on creating a one-stop solution which everyone in your organization can utilize with ease.</p>
@@ -16,7 +16,6 @@ const Hero = () => {
                     <button className='button-primary'>See Our Services</button>
                 </div>
             </div>
-               
             </div>
         </section>
     )
