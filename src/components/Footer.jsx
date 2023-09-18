e55b68358a368b17e4936f7a3eff1f1555ed21b0
Austin O <guitarguy199@gmail.com>
2023-09-18 13:31:47 -0600

    responsive layout and initial styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,25 @@
+import { Link } from 'react-router-dom';
+import Logo from '../assets/tangentlogo.png';
 import './Footer.css';
 
 const Footer = () => {
     return (
-        <h4>Footer</h4>
+        <footer className='footer'>
+            <div className='grid container'>
+                <Link className='footer-logo' to={"/"}>
+                    <img src={Logo} alt="Tangent apps Logo" />
+                </Link>
+                <nav className='footer-list footer-sitemap'>
+                    <h4>Sitemap</h4>
+                </nav>
+                <div className='footer-list footer-contact'>
+                    <h4>Contact Us</h4>
+                </div>
+                <div className=' footer-list footer-socials'>
+                    <h4>Socials</h4>
+                </div>
+            </div>
+        </footer>
     )
 }
 
