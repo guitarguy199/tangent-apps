e87a2016b21fec83bd3d3629bb0f8f0e69d0ca58
Austin O <guitarguy199@gmail.com>
2023-09-18 10:51:33 -0600

    initial commit

--- src/Pages/Home/Intro.jsx
+++ src/Pages/Home/Intro.jsx
@@ -1,8 +1,19 @@
 import "./Intro.css";
+import introImg from './assets/analytics.jpeg';
 
 const Intro = () => {
     return (
-        <h4>Intro</h4>
+        <section className="home-intro">
+        <div className="grid container">
+        <div className="home-intro__text">
+                <h1>Intro header</h1>
+                <p>Intro text</p>
+            </div>
+        <img src={introImg} className="home-intro__img" />
+        </div>
+            
+
+        </section>
     )
 }
 
