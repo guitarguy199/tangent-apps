6c179aeace2943f22e06ef27583378b34695b0f1
Austin O <guitarguy199@gmail.com>
2023-09-18 10:52:03 -0600

    addl packages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,6 +21,7 @@ const Header = () => {
     )
 }
 
+// eslint-disable-next-line react/prop-types
 function CustomLink({ to, children, ...props }) {
     const resolvedPath = useResolvedPath(to)
     const isActive = useMatch({ path: resolvedPath.pathname, end: true})
