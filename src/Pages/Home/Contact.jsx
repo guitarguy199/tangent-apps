967e69f7b401a6194f34ce74dbb81a2b1c84f28d
Austin O <guitarguy199@gmail.com>
2023-09-18 13:32:15 -0600

    initial layout and styling

--- src/Pages/Home/Contact.jsx
+++ src/Pages/Home/Contact.jsx
@@ -2,8 +2,9 @@ import './Contact.css';
 
 const Contact = () => {
     return(
-        <section>
-            <h4>Contact</h4>
+        <section className='grid container home-contact'>
+            <h2>Ready to supercharge your business technology?</h2>
+            <button className='button-primary'>Let&apos;s Chat!</button>
         </section>
     )
 }
