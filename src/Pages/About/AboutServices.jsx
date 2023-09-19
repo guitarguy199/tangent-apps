36bf11d5f8a3db94b42cf1fcb259a616dba79312
Austin O <guitarguy199@gmail.com>
2023-09-19 14:13:55 -0600

    initial build with styling

--- src/Pages/About/AboutServices.jsx
+++ src/Pages/About/AboutServices.jsx
@@ -1,11 +1,35 @@
 import './AboutServices.css'
+import ServiceCard from './ServiceCard';
+import workImg from './assets/client-working.jpeg';
 
 const AboutServices = () => {
+
+    const services = [
+        {
+            img: workImg,
+            name: "Web and App Development",
+            description: "Lorem Ipsum",
+        },
+        {
+            img: workImg,
+            name: "Web and App Development",
+            description: "Lorem Ipsum",
+        },
+        {
+            img: workImg,
+            name: "Web and App Development",
+            description: "Lorem Ipsum",
+        },
+    ]
+
+
     return (
         <section className='about-services'>
             <h1>Our Services</h1>
-            <div className='grid container services-cards'>
-                
+            <div className='grid container'>
+                {services.map((service, index) => 
+                <ServiceCard key={index} service={service} />
+                )}
             </div>
         </section>
     )
