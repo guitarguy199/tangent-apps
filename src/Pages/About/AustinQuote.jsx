a952070cc3af9a3a8119f595003684391f06233e
Austin O <guitarguy199@gmail.com>
2023-09-19 13:22:49 -0600

    initial commit

---
+++ src/Pages/About/AustinQuote.jsx
@@ -0,0 +1,18 @@
+import './AustinQuote.css';
+import austinImg from './assets/austin.jpeg';
+
+const AustinQuote = () => {
+    return (
+            <section className='austin'>
+                <div className='grid container'>
+                    <img src={austinImg} alt="Picture of Founder, Austin" />
+                    <div className='austin-text'>
+                        <h2>What Makes Us Different?</h2>
+                    </div>
+                </div>
+            </section>
+    )
+}
+
+export default AustinQuote;
+
