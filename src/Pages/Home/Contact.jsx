3fd21017bf0a26ede6c939241b759bb64904898a
Austin O <guitarguy199@gmail.com>
2023-09-21 14:41:42 -0600

    add useState hook

--- src/Pages/Home/Contact.jsx
+++ src/Pages/Home/Contact.jsx
@@ -1,6 +1,10 @@
 import './Contact.css';
+import { useState } from 'react';
 
 const Contact = () => {
+
+    const [isFormExpanded, setIsFormExpanded] = useState(false);
+
     return(
         <section className='grid container home-contact'>
             <h2>Ready to supercharge your business technology?</h2>
